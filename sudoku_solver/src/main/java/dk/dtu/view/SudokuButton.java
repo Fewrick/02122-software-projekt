c94fe1d09069a729bdecce5c0ba07406859911f6
Fewrick <frederikudby556@gmail.com>
2024-02-21 15:23:33 +0100

    changes ongoing 5

---
+++ sudoku_solver/src/main/java/dk/dtu/view/SudokuButton.java
@@ -0,0 +1,22 @@
+package dk.dtu.view;
+
+import javafx.scene.control.Button;
+
+public class SudokuButton extends Button {
+
+    private int MyValue;
+
+	public SudokuButton(int MyValue) {
+		this.MyValue = MyValue;
+	}
+
+	// A method to get the value of a button
+	public int getMyValue() {
+		return MyValue;
+	}
+
+	// A method to change the value of a button
+	public void setMyValue(int MyValue) {
+		this.MyValue = MyValue;
+	}
+}
