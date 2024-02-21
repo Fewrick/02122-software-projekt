1fc9d3bf3aa5c3753b5681cc72c17b8176e24fae
michmai <121814049+michmai@users.noreply.github.com>
2024-02-21 13:34:40 +0100

    basicboard seperate class

--- sudoku_solver/src/main/java/dk/dtu/View/SudokuBoard.java
+++ sudoku_solver/src/main/java/dk/dtu/View/SudokuBoard.java
@@ -1,5 +1,7 @@
 package dk.dtu.View;
 
+import java.lang.ModuleLayer.Controller;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
@@ -10,8 +12,8 @@ import javafx.stage.Stage;
 public class SudokuBoard extends Application {
 
     public static Stage boardStage = new Stage();
-    static int sizeX = 600;
-    static int sizeY = 600;
+    static int sizeX = 810;
+    static int sizeY = 810;
     static int gridSize = 9;
     static int btnSize = sizeX / gridSize;
 
@@ -22,45 +24,23 @@ public class SudokuBoard extends Application {
     GridPane pane = new GridPane();
     public static HBox bottom = new HBox();
 
-    public void basicSudoku(){
-        for (int row = 0; row < gridSize; row++) {
-			for (int column = 0; column < gridSize; column++) {
-				SudokuButton Button = new SudokuButton(0);
-				Button.setPrefSize(btnSize, btnSize); // Size of one cell
-
-				/*// Background
-				if (column % 2 == 0 && row % 2 == 0) {
-					Button.setStyle("-fx-base: #8B4513");
-				} else {
-					Button.setStyle("-fx-base: #D2B48C;");
-				}
-				if (column % 2 != 0 && row % 2 != 0) {
-					Button.setStyle("-fx-base: #8B4513");
-				}*/
-
-				pane.add(Button, column, row);
-				buttons2D[row][column] = Button; // Add coordinates and accessibility to all buttons.
-			}
-		}
-    }
-
     @Override
     public void start(Stage stage) throws Exception {
         boardStage = stage;
         boardStage.setTitle("Sudoku game");
 
-        basicSudoku();
+        BasicBoard.basicSudoku(pane);
+        
         borderPane.setBottom(bottom);
         borderPane.setCenter(pane);
         // Constructs pane
         pane.setPrefSize(sizeX, sizeY);
 		pane.setStyle("-fx-background-color: #5DADE2;"); // Sets background color: Green
 
-        
         Scene scene = new Scene(borderPane, sizeX, sizeY);
         boardStage.setScene(scene);
 
-        bottom.setPrefHeight(sizeY/8);
+        bottom.setPrefHeight(sizeY/9);
 
         
 
