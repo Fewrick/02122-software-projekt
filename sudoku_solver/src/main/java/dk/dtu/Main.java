d8231490392aba8950051bfeee9a56861a63fa4f
Fewrick <frederikudby556@gmail.com>
2024-02-21 15:24:16 +0100

    changes ongoing 6

---
+++ sudoku_solver/src/main/java/dk/dtu/Main.java
@@ -0,0 +1,17 @@
+package dk.dtu;
+
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+import dk.dtu.view.App;
+import javafx.application.Application;
+
+public class Main {
+
+    public static void main(String[] args) throws UnknownHostException, SocketException {
+        // // Launch the Lobby Application (start window)
+        System.out.println("Starting application...");
+        Application.launch(App.class, args);
+    
+    }
+}
