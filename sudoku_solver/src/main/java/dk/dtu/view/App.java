ce7ed5eef8dbdcfbfae8c5f39679e14ab2fe65be
ChristofferFrost <114985641+ChristofferFrost@users.noreply.github.com>
2024-02-21 16:23:38 +0100

    MenuExit

--- sudoku_solver/src/main/java/dk/dtu/view/App.java
+++ sudoku_solver/src/main/java/dk/dtu/view/App.java
@@ -27,7 +27,7 @@ public class App extends Application {
 
     private static int sizeX = 700;
     private static int sizeY = 700;
-    public Button Btn = new Button("SUDOKU");
+    public Button ExitBtn = new Button("Exit");
     public Button StartGameBtn = new Button("Start Game");
     public Button Rules = new Button("Rules");
     public static Stage mainMenuStage = new Stage();
@@ -48,14 +48,14 @@ public class App extends Application {
         String buttonStyle = "-fx-background-color: white; -fx-text-fill: black; "
                 + "-fx-font-size: 1.5em; -fx-min-width: 150px; -fx-min-height: 50px; "
                 + "-fx-border-color: black; -fx-border-width: 2px; -fx-border-radius: 5px;";
-        Btn.setStyle(buttonStyle);
+        ExitBtn.setStyle(buttonStyle);
         StartGameBtn.setStyle(buttonStyle);
         Rules.setStyle(buttonStyle);
         
         // Hover effect
         String hoverStyle = "-fx-scale-x: 1.1; -fx-scale-y: 1.1;"; // Enlarge buttons on hover
-        Btn.setOnMouseEntered(e -> Btn.setStyle(buttonStyle + hoverStyle));
-        Btn.setOnMouseExited(e -> Btn.setStyle(buttonStyle));
+        ExitBtn.setOnMouseEntered(e -> ExitBtn.setStyle(buttonStyle + hoverStyle));
+        ExitBtn.setOnMouseExited(e -> ExitBtn.setStyle(buttonStyle));
         StartGameBtn.setOnMouseEntered(e -> StartGameBtn.setStyle(buttonStyle + hoverStyle));
         StartGameBtn.setOnMouseExited(e -> StartGameBtn.setStyle(buttonStyle));
         Rules.setOnMouseEntered(e -> Rules.setStyle(buttonStyle + hoverStyle));
@@ -69,15 +69,18 @@ public class App extends Application {
                 e.printStackTrace();
             }
         });
+        ExitBtn.setOnAction(arg0 -> {
+            mainMenuStage.close();
+        });
         // Layout
         StackPane layout = new StackPane();
         layout.getChildren().add(imageView); 
-        layout.getChildren().addAll(Btn, StartGameBtn, Rules);
+        layout.getChildren().addAll(ExitBtn, StartGameBtn, Rules);
         
-        StackPane.setAlignment(Btn, Pos.CENTER_LEFT);
+        StackPane.setAlignment(ExitBtn, Pos.CENTER_LEFT);
         StackPane.setAlignment(Rules, Pos.CENTER_RIGHT);
         StackPane.setAlignment(StartGameBtn, Pos.CENTER);
-        StackPane.setMargin(Btn, new javafx.geometry.Insets(500, 100, 0, 100)); // Juster top- og venstremarginen efter behov
+        StackPane.setMargin(ExitBtn, new javafx.geometry.Insets(500, 100, 0, 100)); // Juster top- og venstremarginen efter behov
         StackPane.setMargin(Rules, new javafx.geometry.Insets(500, 100, 0, 100)); 
         StackPane.setMargin(StartGameBtn, new javafx.geometry.Insets(500, 0, 0, 0));
 
