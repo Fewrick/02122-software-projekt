208ecd2afa1c191c382a306b37fb907929795418
Fewrick <frederikudby556@gmail.com>
2024-02-21 13:20:08 +0100

    recatr

--- sudoku_solver/src/main/java/dk/dtu/View/Grid.java
+++ sudoku_solver/src/main/java/dk/dtu/View/Grid.java
@@ -1,6 +1,6 @@
 package dk.dtu.View;
 
-public class Board {
+public class Grid {
     public static int board[][] = { {1,2,3,4,5,6,7,8,9},
                                     {1,2,3,4,5,6,7,8,9},
                                     {1,2,3,4,5,6,7,8,9},
