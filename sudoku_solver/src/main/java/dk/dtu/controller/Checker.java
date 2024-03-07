f43039c449428231fc3d1b4971e4cca8967fd803
Fewrick <frederikudby556@gmail.com>
2024-03-07 11:58:49 +0100

    removed redundant code

--- sudoku_solver/src/main/java/dk/dtu/controller/Checker.java
+++ sudoku_solver/src/main/java/dk/dtu/controller/Checker.java
@@ -1,7 +1,5 @@
 package dk.dtu.controller;
 
-import java.util.Arrays;
-
 public class Checker {
 
     public static void boardCompleted(String[][] solvedBoard) {
@@ -22,60 +20,5 @@ public class Checker {
         }
         System.out.println("Board is solved correctly");
         return true;
-    }
-
-    // ------------------------------------------------------------------------------------------------------------------------------------//
-    // This code is mostly copied from the DFS solver, but since the DFS solver is
-    // about to be removed the functions are copied directly.
-    // ------------------------------------------------------------------------------------------------------------------------------------//
-
-    // Checks whether a given number in a cell is in order with the rules
-    private static boolean isSafe(String[][] board, int row, int col, String num) {
-        return !usedInRow(board, row, num) &&
-                !usedInCol(board, col, num) &&
-                !usedInBox(board, row - row % 3, col - col % 3, num);
-    }
-
-    // Checks if the given number is used in the 3x3 box
-    private static boolean usedInBox(String[][] board, int startRow, int startCol, String num) {
-        for (int row = 0; row < 3; row++) {
-            for (int col = 0; col < 3; col++) {
-                if (board[row + startRow][col + startCol].equals(num)) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    // Checks if the given number is used in the row
-    private static boolean usedInRow(String[][] board, int row, String num) {
-        for (int col = 0; col < board.length; col++) {
-            if (board[row][col].equals(num)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    // Checks if the given number is used in the column
-    private static boolean usedInCol(String[][] board, int col, String num) {
-        for (int row = 0; row < board.length; row++) {
-            if (board[row][col].equals(num)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static int[] findEmptyCells(String[][] board) {
-        for (int row = 0; row < board.length; row++) {
-            for (int col = 0; col < board[row].length; col++) {
-                if (board[row][col].equals("")) {
-                    return new int[] { row, col };
-                }
-            }
-        }
-        return null;
     }
 }
