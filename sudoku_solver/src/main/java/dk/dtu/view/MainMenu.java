c14261a367c39e5a9afa3747402c76292caa9bff
Fewrick <frederikudby556@gmail.com>
2024-03-07 20:03:15 +0100

    Update MainMenu.java

--- sudoku_solver/src/main/java/dk/dtu/view/MainMenu.java
+++ sudoku_solver/src/main/java/dk/dtu/view/MainMenu.java
@@ -1,13 +1,6 @@
 package dk.dtu.view;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -15,8 +8,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
+
 import javafx.stage.Stage;
 
 /**
