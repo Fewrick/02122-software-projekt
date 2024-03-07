7b9f4d356318803c5c166d2f99690c159caf07f4
Fewrick <frederikudby556@gmail.com>
2024-03-07 14:26:01 +0100

    Update Leaderboard.java

--- sudoku_solver/src/main/java/dk/dtu/view/Leaderboard.java
+++ sudoku_solver/src/main/java/dk/dtu/view/Leaderboard.java
@@ -10,7 +10,6 @@ import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
