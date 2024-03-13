ad096177de4c89b086f8b017bd32a05e1ff7a468
Fewrick <frederikudby556@gmail.com>
2024-03-13 12:59:02 +0100

    refactor import

--- sudoku_solver/src/main/java/dk/dtu/Main.java
+++ sudoku_solver/src/main/java/dk/dtu/Main.java
@@ -2,9 +2,11 @@ package dk.dtu;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+
+// Used for sql commented section
+// import java.sql.Statement;
+// import java.sql.ResultSet;
 
 import dk.dtu.view.MainMenu;
 import javafx.application.Application;
--- sudoku_solver/src/main/java/dk/dtu/view/Leaderboard.java
+++ sudoku_solver/src/main/java/dk/dtu/view/Leaderboard.java
@@ -7,7 +7,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
