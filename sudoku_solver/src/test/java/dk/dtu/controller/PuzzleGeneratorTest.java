f19ce4420c01b3e0e0cd88cb6abb4494c3f2e730
Fewrick <frederikudby556@gmail.com>
2024-06-14 15:09:40 +0200

    Update Sudoku board handling and test run time
    Code updates modified Sudoku board handling for key press events, button clicks, and the board display process. Additionally, the changes resulted in a slight decrease in test run time. This update improves the game interaction and maintains code efficiency.

--- sudoku_solver/src/test/java/dk/dtu/controller/PuzzleGeneratorTest.java
+++ sudoku_solver/src/test/java/dk/dtu/controller/PuzzleGeneratorTest.java
@@ -45,6 +45,16 @@ public class PuzzleGeneratorTest {
         assertTrue(emptyCells >= 48 && emptyCells <= 63);
     }
 
+    @Test
+    public void testGenerateSudokulevel1() {
+        int[][] sudoku = PuzzleGenerator.generateSudoku("Level 1");
+        assertNotNull(sudoku);
+        assertTrue(sudoku.length == 9);
+
+        int emptyCells = countEmptyCells(sudoku);
+        assertTrue(emptyCells >= 20 && emptyCells <= 45);
+    }
+
     private int countEmptyCells(int[][] sudoku) {
         int count = 0;
         for (int i = 0; i < 9; i++) {
@@ -55,5 +65,46 @@ public class PuzzleGeneratorTest {
             }
         }
         return count;
+    }
+    private int countEmptyCellsBig(int[][] sudoku, int size) {
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (sudoku[i][j] == 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    @Test
+    public void testGenerateBigSudoku3x3() {
+        int[][] sudoku = PuzzleGenerator.generateBigSudoku(3, false);
+        assertNotNull(sudoku);
+        assertTrue(sudoku.length == 9);
+
+        int emptyCells = countEmptyCellsBig(sudoku,9);
+        assertTrue(emptyCells >= 18 && emptyCells <= 41);
+    }
+
+    @Test
+    public void testGenerateBigSudoku4x4() {
+        int[][] sudoku = PuzzleGenerator.generateBigSudoku(4, false);
+        assertNotNull(sudoku);
+        assertTrue(sudoku.length == 16);
+
+        int emptyCells = countEmptyCellsBig(sudoku,16);
+        assertTrue(emptyCells >= 64 && emptyCells <= 128);
+    }
+
+    @Test
+    public void testGenerateBigSudoku5x5() {
+        int[][] sudoku = PuzzleGenerator.generateBigSudoku(5, false);
+        assertNotNull(sudoku);
+        assertTrue(sudoku.length == 25);
+
+        int emptyCells = countEmptyCellsBig(sudoku,25);
+        assertTrue(emptyCells >= 125 && emptyCells <= 313);
     }
 }
