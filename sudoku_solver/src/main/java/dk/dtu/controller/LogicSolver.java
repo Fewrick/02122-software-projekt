9ad064fc20f1b35e53759ff2b20d4026a545e7cf
star3153 <starstrider@duck.com>
2024-06-18 14:11:49 +0200

    Upgraded solver

--- sudoku_solver/src/main/java/dk/dtu/controller/LogicSolver.java
+++ sudoku_solver/src/main/java/dk/dtu/controller/LogicSolver.java
@@ -74,7 +74,6 @@ public class LogicSolver {
     private static BigInteger[][][] solver (int[][] boardInput) {
         BigInteger[][] board = mirrorBoard(boardInput);
         int size = board.length;
-        int boxSize = (int) Math.sqrt(size);
         BigInteger[][][] units = boardUnits(board);
         List<int[]> priority = new ArrayList<int[]>();
         for (int row = 0; row < size; row++) {
@@ -86,41 +85,134 @@ public class LogicSolver {
             }
         }
         
-        BigInteger x;
-        int u;
         int cnt = 0;
         int prioritySizePrev = priority.size();
         int prioritySizeNow = priority.size();
         while (priority.size() > 0) {
             prioritySizePrev = priority.size();
-            for (int h = 0; h < priority.size(); h++) {
-                int row = priority.get(h)[0];
-                int col = priority.get(h)[1];
-
-                // These variables are just here to limit redundant calculations/array lookups
-                x = board[row][col];
-                u = (col/boxSize + (row/boxSize)*boxSize);
-
-                for (int k = 0; k < size; k++) {
-                        x = x.and(units[0][row][k]);
-                        x = x.and(units[1][col][k]);
-                        x = x.and(units[2][u][k]);
-                }
-                board[row][col] = x;
-                if (x.bitCount() == 1) {
-                    units[0][row][col] = x.not();
-                    units[1][col][row] = x.not();
-                    units[2][u][((row - (row/boxSize * boxSize))*boxSize + (col - (col/boxSize * boxSize)))] = x.not();
-                    priority.remove(h);
-                }
-            }
+            if (cnt == 0) {
+                nakedSingles(units, board, priority);
+            } else if (cnt == 1) {
+                hiddenSingles(units, board, priority);
+            } else break;
+            // Can be expanded with more strategies.
+            
             prioritySizeNow = priority.size();
-            if (prioritySizeNow == prioritySizePrev) {
-                if (cnt == 1) break;
-                cnt++;
-            } else cnt = 0;
+            if (prioritySizeNow != prioritySizePrev) {
+                cnt = 0;
+            } else cnt++;
+            System.out.println(priority.size());
         }
         return units;
+    }
+
+    private static void nakedSingles (BigInteger[][][] units, BigInteger[][] board, List<int[]> priority) {
+        int size = board.length;
+        int boxSize = (int) Math.sqrt(size);
+        for (int h = 0; h < priority.size(); h++) {
+            int row = priority.get(h)[0];
+            int col = priority.get(h)[1];
+
+            // These variables are just here to limit redundant calculations/array lookups
+            BigInteger x = board[row][col];
+            int u = (col/boxSize + (row/boxSize)*boxSize);
+
+            for (int k = 0; k < size; k++) {
+                    x = x.and(units[0][row][k]);
+                    x = x.and(units[1][col][k]);
+                    x = x.and(units[2][u][k]);
+            }
+            board[row][col] = x;
+            if (x.bitCount() == 1) {
+                units[0][row][col] = x.not();
+                units[1][col][row] = x.not();
+                units[2][u][((row - (row/boxSize * boxSize))*boxSize + (col - (col/boxSize * boxSize)))] = x.not();
+                priority.remove(h);
+            }
+        }
+    }
+
+
+// Sudoku strategy. Look at the rows, columns and boxes individually and then determine if there is a number that can only be placed in one spot.
+    private static void hiddenSingles (BigInteger[][][] units, BigInteger[][] board, List<int[]> priority) {
+        int size = units[0].length;
+        int boxSize = (int) Math.sqrt(size);
+        int counter = 0, row = 0, col = 0;
+        int unitRow = 0, unitCol = 0, num = 1;
+        int index = 0;
+        
+        while (index < 3 && (unitRow < size || num <= size || unitCol < size)) {
+            BigInteger testVal = valToBinary(num, false);
+            
+            if (unitCol == size) {
+                // System.out.println(index);
+                if (index == 0 && counter == 1) {
+                    
+                    units[0][row][col] = testVal.not();
+                    units[1][col][row] = testVal.not();
+                    units[2][(col/boxSize + (row/boxSize)*boxSize)][((row - (row/boxSize * boxSize))*boxSize + (col - (col/boxSize * boxSize)))] = testVal.not();
+                    board[row][col] = testVal;
+                    System.out.println("row = " + row + ", col = " + col);
+                    priority.remove(findIndex(priority, new int[] {row,col}));
+
+                } else if (index == 1 && counter == 1) {
+                    units[0][col][row] = testVal.not();
+                    units[1][row][col] = testVal.not();
+                    units[2][(row/boxSize + (col/boxSize)*boxSize)][((col - (col/boxSize * boxSize))*boxSize + (row - (row/boxSize * boxSize)))] = testVal.not();
+                    board[col][row] = testVal;
+
+                    priority.remove(findIndex(priority, new int[] {col,row}));
+
+                } else if (index == 2 && counter == 1) {
+                    units[0][(row/boxSize*boxSize+col/boxSize)][((row*boxSize-(row/boxSize*(size)))+(col-(col/boxSize*boxSize)))] = testVal.not();
+                    units[1][((row*boxSize-(row/boxSize*(size)))+(col-(col/boxSize*boxSize)))][(row/boxSize*boxSize+col/boxSize)] = testVal.not();
+                    units[2][row][col] = testVal.not();
+                    board[(row/boxSize*boxSize+col/boxSize)][((row*boxSize-(row/boxSize*(size)))+(col-(col/boxSize*boxSize)))] = testVal;
+
+                    priority.remove(findIndex(priority, new int[] {(row/boxSize*boxSize+col/boxSize),((row*boxSize-(row/boxSize*(size)))+(col-(col/boxSize*boxSize)))}));
+
+                }
+                num++;
+                unitCol = 0;
+                counter = 0;
+                continue;
+            } else if (num > size) {
+                unitRow++;
+                num = 1;
+                continue;
+            } else if (unitRow == size) {
+                index++;
+                unitRow = 0;
+            } else if ( (index == 0 && board[unitRow][unitCol].bitCount() > 1 && board[unitRow][unitCol].and(testVal).compareTo(BigInteger.ZERO) != 0) || 
+                        (index == 1 && board[unitCol][unitRow].bitCount() > 1 && board[unitCol][unitRow].and(testVal).compareTo(BigInteger.ZERO) != 0) ||
+                        (index == 2 && board[(unitRow/boxSize*boxSize+unitCol/boxSize)][((unitRow*boxSize-(unitRow/boxSize*(size)))+(unitCol-(unitCol/boxSize*boxSize)))].bitCount() > 1 && board[(unitRow/boxSize*boxSize+unitCol/boxSize)][((unitRow*boxSize-(unitRow/boxSize*(size)))+(unitCol-(unitCol/boxSize*boxSize)))].and(testVal).compareTo(BigInteger.ZERO) != 0)) {
+                counter++;
+                row = unitRow;
+                col = unitCol;
+            }
+            unitCol++;
+        }
+
+    }
+
+    // NEEDED FOR INDEXING IN HIDDENSINGLES
+    private static int findIndex (List<int[]> list, int[] value) {
+        for (int i = 0; i < list.size(); i++) {
+            if (customEquals(list.get(i), value)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    // NEEDED FOR INDEXING IN HIDDENSINGLES
+    private static boolean customEquals (int[] listElement, int[] value) {
+        if (listElement.length > 2 || value.length > 2) {
+            System.out.println("ERROR - ARRAY TO BIG");
+            return false;
+        }
+        if (listElement[0] == value[0] && listElement[1] == value[1]) {
+            return true;
+        } else return false;
     }
 
 // Takes a finsihed sudoku and checks that all the elements in each unit-row is unique, i.e. checks that the sudoku is valid
